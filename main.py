b5e9f98f172b6ccb9a08eb7b401692094d1d72b4
Anton <popovine@google.com>
2023-11-20 15:56:34 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    country = request.args.get("country")
+    if country:
+        return f"Welcome to {country}!"
+    else:
+        return "Please provide a country as input."
+
+if __name__ == "__main__":
+    app.run(port=8080)
