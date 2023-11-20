1ded528ca9709eb9ae3d491bdaf72db67f7c7a7e
Anton <popovine@google.com>
2023-11-20 16:06:30 +0000

    add llm

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from flask import Flask, request
+import vertexai
+from vertexai.language_models import TextGenerationModel
 
+vertexai.init(project="gke-dja-demo", location="europe-west4")
+parameters = {
+    "candidate_count": 1,
+    "max_output_tokens": 2048,
+    "temperature": 0.2,
+    "top_p": 0.8,
+    "top_k": 40
+}
+model = TextGenerationModel.from_pretrained("text-bison")
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
     country = request.args.get("country")
     if country:
-        return f"Welcome to {country}!!!!"
+        response = model.predict(
+            """Write me a 3 day trip plan for {}.""".format(country),
+            **parameters
+        )
+        resp = response.text
+        return resp
     else:
         return "Please provide a country as input."
 
